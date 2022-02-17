1dce29d4710b295ed75b5251425bdcbe59b97474
Douglas-Santana <douguinhodns@gmail.com>
2022-02-16 21:58:07 -0300

    :sparkles: Inicia frontend

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Minha Lista de Tarefas</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className='addTask'>
+      <input type='text'/>
+      <button>Adicionar</button>
+    </div>
+  );
+}
+
+export default Main;
