ee9d799f265acac35dfab94b6a8a65b8f8d0b89a
Douglas-Santana <douguinhodns@gmail.com>
2022-02-17 11:03:34 -0300

    :construction: adiciona componente de funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Funcionalities from './components/Funcionalities';
 import Header from './components/Header';
 import Main from './components/Main';
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Funcionalities />
     </>
   );
 }
---
+++ src/components/Funcionalities/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Funcionalities() {
+  return (
+    <>
+      <button>Excluir</button>
+    </>
+  );
+}
+
+export default Funcionalities;
