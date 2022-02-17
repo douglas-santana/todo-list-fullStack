2fe5a49f2759fb8b47c36d5f8686f8f045255a2c
Douglas-Santana <douguinhodns@gmail.com>
2022-02-17 09:14:16 -0300

    :sparkles: adiciona funcionalidade que manda os dados do input para o backend

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Main() {
+  const [task, setTask] = useState('');
+  const [arrTasks, setArrTasks] = useState([]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setTask(value);
+  }
+
+  const addTaskonList = async () => {
+    setArrTasks([...arrTasks, task]);
+    await axios.post('http://localhost:3001/', { task });
+  }
+
+  const getTasks = async () => {
+    const { data: { tasks: arrTasksSaved }} = await axios.get('http://localhost:3001/');
+    setArrTasks(arrTasksSaved);
+  }
+
+  useEffect(() => {
+    getTasks();
+  }, [])
+
   return (
-    <div className='addTask'>
-      <input type='text'/>
-      <button>Adicionar</button>
-    </div>
+    <>
+      <div className='addTask'>
+        <input type='text' onChange={handleChange}/>
+        <button onClick={addTaskonList}>Adicionar</button>
+      </div>
+      <div>
+        <ul>{arrTasks.map((task) => <li key={task}>{task}</li>)}</ul>
+      </div>
+    </>
   );
 }
 
